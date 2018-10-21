21570ed4ffb817318e613f6b8f9f2bfb1e9d9b19
Kumar Gatlewar <SANKUM@EUR.AD.SAG>
2018-10-21 23:23:33 +0530

    CE is working without webcomponentsjs

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -2,7 +2,7 @@
 */
 
 /** IE9, IE10 and IE11 requires all of the following polyfills. **/
-import 'core-js/es6/symbol';
+ import 'core-js/es6/symbol';
 import 'core-js/es6/object';
 import 'core-js/es6/function';
 import 'core-js/es6/parse-int';
@@ -13,9 +13,9 @@ import 'core-js/es6/string';
 import 'core-js/es6/date';
 import 'core-js/es6/array';
 import 'core-js/es6/regexp';
-import 'core-js/es6/map';
-import 'core-js/es6/weak-map';
-import 'core-js/es6/set';
+// import 'core-js/es6/map';
+// import 'core-js/es6/weak-map';
+// import 'core-js/es6/set';
 import 'core-js/es6/reflect';
 import 'core-js/client/shim';
 
@@ -23,12 +23,12 @@ import 'core-js/client/shim';
 import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** IE10 and IE11 requires the following to support `@angular/animation`. */
-import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /***************************************************************************************************
 * Zone JS is required by Angular itself.
 */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+ import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 /***************************************************************************************************
 * APPLICATION IMPORTS
@@ -50,5 +50,5 @@ import 'intl/locale-data/jsonp/en.js';
 if (typeof SVGElement.prototype.contains == 'undefined') {
     SVGElement.prototype.contains = HTMLDivElement.prototype.contains;
 }
-// webcomponents-bundle is not required
+//import '@webcomponents/webcomponentsjs/webcomponents-loader.js';
 import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';
